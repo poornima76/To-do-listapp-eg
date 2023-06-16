e0551f072221b5838b9762a42b3f0e11ec5cc1ab
Poornima Marasini <poornima.mrsn76@gmail.com>
2023-06-16 13:57:47 +0545

    setting up

---
+++ test.js
@@ -0,0 +1,10 @@
+// Using var 
+var a = '123';
+console.log(a);
+
+//Using let
+let b = '456';
+console.log(b);
+
+const c = 789;
+console.log(c);
---
+++ test/index.js
@@ -0,0 +1,15 @@
+// Using var 
+var a = '123';
+console.log(a);
+
+//Using let
+let b = 456;
+console.log(b);
+
+const c = 789;
+console.log(c);
+// alert('hello');
+// let age = prompt('How old r u?');
+// console.log(age);
+const isOld = confirm("R u old?");
+console.log(isOld);
